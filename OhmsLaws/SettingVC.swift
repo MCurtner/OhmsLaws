89437dc1a169a1a4b3383d96d4b25b5007bf02ea
Matthew <Kinematic12@gmail.com>
2016-09-05 20:31:28 +0200

    Adding IAP for Ad Removal and Purchase restore

--- OhmsLaws/SettingVC.swift
+++ OhmsLaws/SettingVC.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import StoreKit
 
 class SettingVC: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var roundingTextField: UITextField!
+    
+    var productIdentifer = Set(["com.matthewcurtner.AdRemovalTest", "com.matthewcurtner.invalidTest"])
+    var product: SKProduct?
+    var productsArray: [SKProduct] = []
     
     var roundingDigit: Int!
 
@@ -18,8 +23,15 @@ class SettingVC: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         roundingTextField.delegate = self
         
+        // Load the user defaults
         readUserDefaults()
         roundingTextField.text = "\(roundingDigit!)"
+        
+        // Re
+        requestProductData()
+        
+        // Register with delegate
+        SKPaymentQueue.default().add(self)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -29,7 +41,7 @@ class SettingVC: UIViewController, UITextFieldDelegate {
         // be dismissed.
         self.view.endEditing(true)
     }
-
+    
     // MARK: - UITextField Delegate Method
     /// Prevent user from inserting more than 1 character
     /// in the roundingTextField.
@@ -48,12 +60,13 @@ class SettingVC: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: - NSUserDefaults
-    /// Retrieve the User/Default values for view and editing
+    /// Retrieve the User/Default values for view
     func readUserDefaults() {
         let defaults = UserDefaults.standard
         roundingDigit = defaults.integer(forKey: "RoundTo")
     }
     
+    /// Set the User/Default values for rounding decimal
     func setUserDefaults() {
         let defaults = UserDefaults.standard
         roundingDigit = Int(roundingTextField.text!)
@@ -61,24 +74,124 @@ class SettingVC: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: - Actions
+    /// Dismiss Setting VC without saving
     @IBAction func cancelBtnWasPressed(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
 
+    /// Dismiss Setting VC while saving changes
     @IBAction func saveBtnWasPressed(_ sender: AnyObject) {
         setUserDefaults()
         dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    /// Restore Purchases
+    @IBAction func removeAdsBtnWasPressed(_ sender: UIButton) {
+        let payment = SKPayment(product: productsArray[0])
+        SKPaymentQueue.default().add(payment)
     }
-    */
+    
+    /// Restore Purchases
+    @IBAction func restorePurchases(_ sender: UIButton) {
+        SKPaymentQueue.default().add(self)
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+}
 
+extension SettingVC: SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    /// Determine if the users device has In-App Purchases enabled,
+    /// and if so, start the request for available purchases.  
+    /// If IAP is not enabled, display message to user.
+    func requestProductData() {
+        if SKPaymentQueue.canMakePayments() {
+            print("Success")
+            let request = SKProductsRequest(productIdentifiers: self.productIdentifer)
+            request.delegate = self
+            request.start()
+        } else {
+            // Show Alert stating IAP are not enabled.
+            let alert = UIAlertController(title: "In-App Purchases Not Enabled",
+                                          message: "Please enable In App Purchases in Settings",
+                                          preferredStyle: .alert)
+            
+            // Add Action 'Settings' to open settings
+            alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { (alertAction) in
+                alert.dismiss(animated: true, completion: nil)
+                
+                let url: URL? = URL(fileURLWithPath: UIApplicationOpenSettingsURLString)
+                if url != nil {
+                    UIApplication.shared.openURL(url!)
+                }
+            }))
+            
+            // Add Action 'OK' to dismiss alert
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (alertAction) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+        }
+    }
+    
+    
+    /// SKProductsRequestDelegate Method
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        // Retrieve the response of products
+        var products = response.products
+        
+        // Loop through and store each product in the
+        // productsArray.
+        if products.count != 0 {
+            for i in 0 ..< products.count {
+                self.product = products[i] as SKProduct
+                self.productsArray.append(product!)
+            }
+        } else {
+            print("No Products Found")
+        }
+        
+        // Store any invalid product identifiers
+        let prod = response.invalidProductIdentifiers
+        
+        // Print out the list of invalid identifiers
+        for product in prod {
+            print("\(product) was not found")
+        }
+    }
+
+    /// Transaction of payment
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions as [SKPaymentTransaction] {
+            switch transaction.transactionState {
+            case .purchased:
+                print("Transaction Approved")
+                print("Product ID: \(transaction.payment.productIdentifier)")
+                self.deliverProduct(transaction: transaction)
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .failed:
+                print("Transaction Failed")
+                SKPaymentQueue.default().finishTransaction(transaction)
+            default:
+                break
+            }
+        }
+    }
+    
+    func deliverProduct(transaction: SKPaymentTransaction) {
+        if transaction.payment.productIdentifier == "com.matthewcurtner.AdRemovalTest" {
+            print("Non-Consumable Product Purchased")
+            // Unlock Feature
+        }
+    }
+
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        print("Transactions Restored")
+        for transaction: SKPaymentTransaction in queue.transactions {
+            if transaction.payment.productIdentifier == "com.matthewcurtner.AdRemovalTest" {
+                print("Non-Consumable Product Purchased")
+                // Unlock Feature
+                
+            }
+        }
+    }
 }
 
